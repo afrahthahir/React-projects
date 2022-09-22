6a5707bb296cf673d03fb2bc7c18e31e97fa535c
afrahthahir <55926806+afrahthahir@users.noreply.github.com>
2022-09-22 14:10:57 +0530

    Add files via upload

---
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -0,0 +1,49 @@
+import React, { useState, useRef, useEffect } from "react";
+import { FaExternalLinkAlt } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+
+const Submenu = () => {
+  const {
+    isSubmenuOpen,
+    page: { page, links },
+    location,
+  } = useGlobalContext();
+  const container = useRef(null);
+  const [columns, setColumns] = useState("col-2");
+
+  useEffect(() => {
+    setColumns("col-2");
+    const submenu = container.current;
+    const { center, bottom } = location;
+    submenu.style.left = `${center}px`;
+    submenu.style.top = `${bottom}px`;
+
+    if (links.length === 3) {
+      setColumns("col-3");
+    }
+    if (links.length > 3) {
+      setColumns("col-4");
+    }
+  }, [location, links]);
+  return (
+    <aside
+      className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}
+      ref={container}
+    >
+      <h4>{page}</h4>
+      <div className={`submenu-center ${columns}`}>
+        {links.map((link) => {
+          const { label, url, icon } = link;
+          return (
+            <a href={url}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
+    </aside>
+  );
+};
+
+export default Submenu;
